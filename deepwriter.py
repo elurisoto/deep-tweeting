01b416f7507ae18b977a3ac653f472f4e5e2e127
Elu Risoto <elurisoto@gmail.com>
2018-08-17 14:24:08 +0200

    Create deepwriter class, move back tweet related functions to perezrebote.py

---
+++ deepwriter.py
@@ -0,0 +1,134 @@
+import json
+import time
+import random
+import sys
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import LSTM
+from keras.utils import np_utils
+from keras.callbacks import Callback
+
+# based on https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py
+
+class DeepWriter(Callback):
+    def __init__(self, x, y, layers, neurons, max_len, char_indices, indices_char, chars, modelname, text, ):
+        self.x = x
+        self.y = y
+        self.layers = layers
+        self.neurons = neurons
+        self.max_len = max_len
+        self.char_indices = char_indices
+        self.indices_char = indices_char
+        self.chars = chars
+        self.modelname = modelname
+        self.text = text
+
+        model = Sequential()
+        model.add(LSTM(self.neurons, input_shape=(self.x.shape[1], self.x.shape[2]), return_sequences=True))
+        model.add(Dropout(0.2))
+        for _ in range(self.layers-1):
+            model.add(LSTM(self.neurons))
+            model.add(Dropout(0.2))
+        if self.layers==1:
+            model.add(Flatten())
+        model.add(Dense(self.y.shape[1], activation='softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+        self.model = model
+
+    def train(self, epochs, batch_size=100):
+        "Trains the model for the specified number of epochs"
+        self.model.fit(self.x, self.y, epochs=epochs, batch_size=batch_size, callbacks=[self])
+        filename = ([time.strftime("%Y%m%d-%H%M%S"), self.modelname] + 
+                    ["{}_0.2".format(self.neurons)]*self.layers + 
+                    ["{}_ep".format(epochs)])
+        filename = '_'.join(filename)
+        self.model.save_weights('models/{}.h5'.format(filename))
+
+    @staticmethod
+    def vectorize(text, max_len=40, step=1):
+        "Converts the corpus to the structure required by the model"
+        chars = sorted(list(set(full_text)))
+        print('total chars:', len(chars))
+        char_indices = dict((c, i) for i, c in enumerate(chars))
+        indices_char = dict((i, c) for i, c in enumerate(chars))
+
+        # cut the text in semi-redundant sequences of maxlen characters
+        sentences = []
+        next_chars = []
+        for i in range(0, len(full_text) - max_len, step):
+            sentences.append(full_text[i: i + max_len])
+            next_chars.append(full_text[i + max_len])
+
+        x = np.zeros((len(sentences), max_len, len(chars)), dtype=np.bool)
+        y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+        for i, sentence in enumerate(sentences):
+            for t, char in enumerate(sentence):
+                x[i, t, char_indices[char]] = 1
+            y[i, char_indices[next_chars[i]]] = 1
+
+        return {
+            'char_indices': char_indices, 
+            'indices_char': indices_char, 
+            'chars': chars,
+            'x': x, 
+            'y': y,
+            'max_len': max_len,
+            "text": text
+        }
+
+    @staticmethod
+    def sample(preds, temperature=1.0):
+        "Helper function to sample an index from a probability array"
+        preds = np.asarray(preds).astype('float64')
+        preds = np.log(preds) / temperature
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
+
+
+    def on_epoch_end(self, epoch, logs):
+        "Function invoked at end of each epoch. Prints generated text."
+        print()
+        print('----- Generating text after Epoch: %d' % epoch)
+        self.talk(400, [0.2, 0.5, 1.0, 1.2])
+
+
+    def talk(self, n_chars, diversity=1):
+        "Generates a message of n_chars"
+        if isinstance(diversity, list):
+            diversities = diversity
+        else:
+            diversities = [diversity]
+
+        start_index = random.randint(0, len(full_text) - self.max_len - 1)
+        for diversity in diversities:
+            print('----- diversity:', diversity)
+
+            generated = ''
+            sentence = full_text[start_index: start_index + self.max_len]
+            generated += sentence
+            print('----- Generating with seed: "' + sentence + '"')
+            sys.stdout.write(generated)
+
+            for _ in range(400):
+                x_pred = np.zeros((1, self.max_len, len(self.chars)))
+                for t, char in enumerate(sentence):
+                    x_pred[0, t, self.char_indices[char]] = 1.
+
+                preds = self.model.predict(x_pred, verbose=0)[0]
+                next_index = self.sample(preds, diversity)
+                next_char = self.indices_char[next_index]
+
+                generated += next_char
+                sentence = sentence[1:] + next_char
+
+                sys.stdout.write(next_char)
+                sys.stdout.flush()
+            print()
--- perezrebote.py
+++ perezrebote.py
@@ -1,41 +1,56 @@
 import re
+import configparser
+import tweepy
 import json
-import numpy as np
-import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers import LSTM
-from keras.utils import np_utils
-from get_tweets import get_tweets
+from tqdm import tqdm
+from deepwriter import DeepWriter
 
-# Neural network part based on https://www.analyticsvidhya.com/blog/2018/03/text-generation-using-python-nlp/
+def api_login(key_cfg):
+    keys = configparser.ConfigParser()
+    keys.read(key_cfg)
+
+    access_token = keys.get("general", "access_token")
+    access_token_secret = keys.get("general", "access_token_secret")
+    consumer_key = keys.get("general", "consumer_key")
+    consumer_secret = keys.get("general", "consumer_secret")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+def get_tweets(twitter_handle, num_tweets, outfile=None, key_cfg="api-key.cfg"):
+    api = api_login(key_cfg)
+    tweets = []
+    last_tweet = None
+    with tqdm(total=num_tweets) as pbar:
+        while len(tweets) < num_tweets:
+            new_tweets = api.user_timeline(twitter_handle, 
+                                           count=(num_tweets-len(tweets))%500,
+                                           max_id=last_tweet,
+                                           tweet_mode='extended')
+            if len(new_tweets) > 0:
+                tweets.extend(new_tweets)
+                pbar.update(len(new_tweets))
+                last_tweet = tweets[-1]._json["id"] - 1
+            else:
+                break
+
+    print("Downloaded {} tweets.".format(len(tweets)))
+    tweets = [tweet._json for tweet in tweets]
+
+    if outfile:
+        with open(outfile, 'w') as fout:
+            json.dump(tweets, fout)
+
+    return tweets
+
 
 def clean(tweet, fields={'full_text', 'id'}):
     clean_tweet = {k:v for k,v in tweet.items() if k in fields}
     clean_tweet['is_retweet'] = 'retweeted_status' in tweet
     clean_tweet['text'] = re.sub(r"@(\w){1,15}", '', clean_tweet['full_text']).strip()
     return clean_tweet
-    
-def train_model(X, Y, layers=2, neurons=400, epochs=1, modelname="reverte", batch_size=100):
-    model = Sequential()
-    model.add(LSTM(neurons, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-    model.add(Dropout(0.2))
-    for i in range(layers-1):
-        model.add(LSTM(neurons))
-        model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(Y.shape[1], activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-    model.fit(X, Y, epochs=epochs, batch_size=batch_size)
-
-    filename = [modelname] + ["{}_0.2".format(neurons)]*layers
-    filename = '_'.join(filename)
-    model.save_weights('models/{}.h5'.format(filename))
-    return model
 
 if __name__ == "__main__":
     # get_tweets("perezreverte", 4000, "reverte.json")
@@ -47,44 +62,13 @@ if __name__ == "__main__":
     corpus = [tweet['text'] for tweet in clean_tweets if not tweet['is_retweet']]
     full_text = '\n'.join(corpus).lower()
 
-    characters = sorted(list(set(full_text)))
-    n_to_char = {n:char for n, char in enumerate(characters)}
-    char_to_n = {char:n for n, char in enumerate(characters)}
+    model_config = DeepWriter.vectorize(full_text)
 
-    X = []
-    Y = []
-    length = len(full_text)
-    seq_length = 100
+    model_config.update({
+        "layers": 1,
+        "neurons": 3,
+        "modelname": "reverte"
+    })
 
-    for i in range(0, length-seq_length, 1):
-        sequence = full_text[i:i + seq_length]
-        label =full_text[i + seq_length]
-        X.append([char_to_n[char] for char in sequence])
-        Y.append(char_to_n[label])
-
-    X_modified = np.reshape(X, (len(X), seq_length, 1))
-    X_modified = X_modified / float(len(characters))
-    Y_modified = np_utils.to_categorical(Y)
-
-    model_config = {
-        "layers": 2,
-        "neurons": 512,
-        "epochs": 100
-    }
-    model = train_model(X_modified, Y_modified, **model_config)
-
-    string_mapped  = X[99]
-    full_string = [n_to_char[value] for value in string_mapped]
-    # generating characters
-    for i in range(280):
-        x = np.reshape(string_mapped,(1,len(string_mapped), 1))
-        x = x / float(len(characters))
-
-        pred_index = np.argmax(model.predict(x, verbose=0))
-        seq = [n_to_char[value] for value in string_mapped]
-        full_string.append(n_to_char[pred_index])
-
-        string_mapped.append(pred_index)
-        string_mapped = string_mapped[1:len(string_mapped)]
-
-    print(''.join(full_string))
+    model = DeepWriter(**model_config)
+    model.train(1)
